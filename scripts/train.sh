e838c47ad715ece986fa61fceaed9cefc242cefd
whr94621@163.com <whr94621@163.com>
2018-04-03 21:18:40 +0800

    Add train.sh.

---
+++ scripts/train.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+export CUDA_VISIBLE_DEVICES=0
+
+echo "Using GPU $CUDA_VISIBLE_DEVICES..."
+
+python ../train.py \
+    --model_name "transformer" \
+    --reload \
+    --config_path "../configs/noam_schedule_config.yaml" \
+    --log_path "./log" \
+    --saveto "./save/" \
+    --use_gpu
