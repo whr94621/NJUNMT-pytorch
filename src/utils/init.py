12d7bb7aa770d3c46791ba13ea8d6e03febb4ee7
whr94621 <whr94621@qq.com>
2018-12-13 18:41:36 +0800

    remove hybrid_nmt
    Signed-off-by: whr94621 <whr94621@qq.com>

--- src/utils/init.py
+++ src/utils/init.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 
+
 def default_init(tensor):
     if tensor.ndimension() == 1:
         nn.init.constant_(tensor, val=0.0)
@@ -9,16 +10,16 @@ def default_init(tensor):
 
     return tensor
 
+
 def embedding_init(tensor, val=0.1):
     nn.init.uniform_(tensor, -val, val)
 
     return tensor
 
+
 def rnn_init(tensor):
     if tensor.ndimension() != 2:
         return default_init(tensor)
-
-    return default_init(tensor)
 
     r, c = tensor.size()
 
@@ -36,11 +37,9 @@ def rnn_init(tensor):
     sub_tensors = [torch.Tensor(*sub_size).normal_(0, 1) for _ in range(n)]
     sub_tensors = [torch.svd(w, some=True)[0] for w in sub_tensors]
 
-    init_tensor = torch.cat(sub_tensors, dim=dim) # [r, c]
+    init_tensor = torch.cat(sub_tensors, dim=dim)  # [r, c]
 
     with torch.no_grad():  # inplace op should be wrapped in no_grad mode.
         tensor.copy_(init_tensor)
 
     return tensor
-
-
